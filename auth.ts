ee84c6f7c76da5fc3ad4b06b5aa83f75fa92853d
Justin Abraham Ipe <justinabrahamipe@gmail.com>
2025-11-17 14:49:20 +0000

    Enable auth debug mode for troubleshooting

--- auth.ts
+++ auth.ts
@@ -22,7 +22,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     }),
   ],
   trustHost: true,
-  debug: false,
+  debug: true,
   pages: {
     signIn: "/login",
     verifyRequest: "/verify-request",
